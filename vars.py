c80ab6e2a0072b41f23371d62fd23218005a34f9
NamasteCS <963ajp@gmail.com>
2025-08-01 21:05:55 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,13 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "xxxxxxxx"))
+API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxx")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+OWNER = int(environ.get("OWNER", "6978232852"))
+CREDIT = "<blockquote>[𝗖𝗛𝗢𝗦𝗘𝗡 𝗢𝗡𝗘 ⚝](http://t.me/CHOSEN_ONEx_bot)</blockquote>"
+LOG_CHANNEL = "-1002460920533" # log group me bot ko admin narur banaye full rights ke sath
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
