586c67ca1c668e340c3f80b698b961882accf29f
rockybhai2109 <r.kingbhai786@gmail.com>
2025-08-08 12:45:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1003,18 +1003,18 @@ async def txt_handler(bot: Client, m: Message):
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
 
-            elif "classplusapp" in url:
-                signed_api = f"https://cpapi-rjbs.onrender.com/extract_keys?url={url}@bots_updatee&user_id={7687410009}"
-                response = requests.get(signed_api, timeout=20)
+           # elif "classplusapp" in url:
+              #  signed_api = f"https://cpapi-rjbs.onrender.com/extract_keys?url={url}@bots_updatee&user_id={7687410009}"
+              #  response = requests.get(signed_api, timeout=20)
                 #url = response.text.strip()
-                url = response.json()['url']  
+             #   url = response.json()['url']  
 
-            elif "classplusapp.com/drm/" in url:
-                url = f"https://drmapijion-botupdatevip.vercel.app/api?url={url}&token={raw_text4}"
+          #  elif "classplusapp.com/drm/" in url:
+              #  url = f"https://drmapijion-botupdatevip.vercel.app/api?url={url}&token={raw_text4}"
                 #url = 'https://dragoapi.vercel.app/classplus?link=' + url
-                mpd, keys = helper.get_mps_and_keys(url)
-                url = mpd
-                keys_string = " ".join([f"--key {key}" for key in keys])
+               # mpd, keys = helper.get_mps_and_keys(url)
+               # url = mpd
+                #keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "tencdn.classplusapp" in url:
                 headers = {'host': 'api.classplusapp.com', 'x-access-token': f'{raw_text4}', 'accept-language': 'EN', 'api-version': '18', 'app-version': '1.4.73.2', 'build-number': '35', 'connection': 'Keep-Alive', 'content-type': 'application/json', 'device-details': 'Xiaomi_Redmi 7_SDK-32', 'device-id': 'c28d3cb16bbdac01', 'region': 'IN', 'user-agent': 'Mobile-Android', 'webengage-luid': '00000187-6fe4-5d41-a530-26186858be4c', 'accept-encoding': 'gzip'}
@@ -1380,6 +1380,7 @@ async def text_handler(bot: Client, m: Message):
             elif "https://cpvod.testbook.com/" in url:
                 url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/")
                 url = f"https://cpapi-rjbs-1l0p.onrender.com/extract_keys?url={url}@bots_updatee&user_id={7687410009}"
+               # url = f"https://scammer-keys.vercel.app/api?url={url}&token={cptoken}&auth=@scammer_botxz1"
                 mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
                 keys_string = " ".join([f"--key {key}" for key in keys])
@@ -1390,15 +1391,15 @@ async def text_handler(bot: Client, m: Message):
             elif "classplusapp" in url:
                 signed_api = f"https://cpapi-rjbs-1l0p.onrender.com/extract_keys?url={url}@bots_updatee&user_id={7687410009}"
                 response = requests.get(signed_api, timeout=20)
-                #url = response.text.strip()
+                url = response.text.strip()
                 url = response.json()['url']  
 
-            elif "classplusapp.com/drm/" in url:
-                url = f"https://drmapijion-botupdatevip.vercel.app/api?url={url}&token={raw_text4}"
+          #  elif "classplusapp.com/drm/" in url:
+         #       url = f"https://drmapijion-botupdatevip.vercel.app/api?url={url}&token={raw_text4}"
                 #url = 'https://dragoapi.vercel.app/classplus?link=' + url
-                mpd, keys = helper.get_mps_and_keys(url)
-                url = mpd
-                keys_string = " ".join([f"--key {key}" for key in keys])
+         #       mpd, keys = helper.get_mps_and_keys(url)
+        #        url = mpd
+        #        keys_string = " ".join([f"--key {key}" for key in keys])
 
 
 
